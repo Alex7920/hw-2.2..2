7aefeba798d5fe9ad5ca9854f9d5f8e4b8d8df7a
Александр <kuzya-79@yandex.ru>
2022-11-09 13:16:35 +0300

    сделано дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car("car", 4 );
+        Truck truck = new Truck( "truck1", 6);
+        Bicycle bicycle = new Bicycle("bicycle", 2);
+
+        ServiceStation station = new ServiceStation();
+        station.check(car);
+        System.out.println();
+        station.check(bicycle);
+        System.out.println();
+        station.check(truck);
+        System.out.println();
+
     }
 }
